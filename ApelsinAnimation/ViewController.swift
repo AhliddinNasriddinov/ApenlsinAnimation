16e7eaa90d5588f668faf5b95ab71dac6c8b4f5b
Nasriddinov Ahliddin <nasriddinovahliddin001@gmail.com>
2022-04-13 18:33:15 +0500

    Initial Commit

---
+++ ApelsinAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApelsinAnimation
+//
+//  Created by Ahliddin Nasriddinov on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
